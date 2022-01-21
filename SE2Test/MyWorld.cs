0e100db9faed29ddfaf2e5c6f37f25914fa37f95
Alexis Huvier <alexis.huvier@alumni.univ-avignon.fr>
2022-01-21 17:55:06 +0100

    Beginning of physics engine implementation

--- SE2Test/MyWorld.cs
+++ SE2Test/MyWorld.cs
@@ -4,13 +4,13 @@ using SE2.Components;
 using SE2.Widgets;
 using SE2.Utils;
 using System.Collections.Generic;
-using System.Linq;
 
 namespace SE2Test
 {
     class MyWorld: Scene
     {
         readonly Entity e5;
+        readonly Entity e6;
 
         public MyWorld(): base()
         {
@@ -19,11 +19,6 @@ namespace SE2Test
             e2.AddComponent(new TileMapComponent("tiled/map.tmx"));
             AddEntity(e2);
 
-            Entity e3 = new Entity();
-            e3.AddComponent(new TransformComponent(new Vec3(600, 450), new Vec3(0.5f)));
-            e3.AddComponent(new SpriteComponent("awesomeface"));
-            AddEntity(e3);
-
             e5 = new Entity();
             e5.AddComponent(new TransformComponent(new Vec3(600, 450), new Vec3(3)));
             e5.AddComponent(new SpriteSheetComponent("spritesheet", new Vec2(32), new Dictionary<string, List<int>>() { { "idle", new List<int>() { 13, 14, 15, 16, 17, 18, 19, 20 } } }, "idle", 150));
@@ -31,15 +26,17 @@ namespace SE2Test
             e5.AddComponent(new RectCollisionComponent(new Vec3(96, 96, 1), new Vec3(0), true));
             AddEntity(e5);
 
-            Entity e6 = new Entity();
-            e6.AddComponent(new TransformComponent(new Vec3(700, 450), new Vec3(3)));
-            e6.AddComponent(new SpriteSheetComponent("spritesheet", new Vec2(32), new Dictionary<string, List<int>>() { { "idle", new List<int>() { 13, 14, 15, 16, 17, 18, 19, 20 } } }, "idle", 150));
-            AddEntity(e6);
+            Entity e7 = new Entity();
+            e7.AddComponent(new TransformComponent(new Vec3(600, 450)));
+            e7.AddComponent(new RectComponent(new Vec3(200), Color.BLUE));
+            e7.AddComponent(new Physics2DComponent(new Vec2(200), 1, tainicom.Aether.Physics2D.Dynamics.BodyType.Kinematic));
+            AddEntity(e7);
 
-            Entity e4 = new Entity();
-            e4.AddComponent(new TransformComponent(new Vec3(600, 450)));
-            e4.AddComponent(new TextComponent("SALUT !", "basic"));
-            AddEntity(e4);
+            e6 = new Entity();
+            e6.AddComponent(new TransformComponent(new Vec3(600, 700), 35));
+            e6.AddComponent(new RectComponent(new Vec3(20), Color.RED));
+            e6.AddComponent(new Physics2DComponent(new Vec2(20), 10));
+            AddEntity(e6);
 
             AddWidget(new Image(new Vec3(100, 100), new Vec3(1), "awesomeface"));
             AddWidget(new Label(new Vec3(100, 100), new Vec3(1), "Heyo !", "basic"));
@@ -53,6 +50,9 @@ namespace SE2Test
                 e5.GetComponent<SpriteSheetComponent>().flipX = !e5.GetComponent<SpriteSheetComponent>().flipX;
             if (GetWindow().inputManager.IsKeyPressed(Inputs.Key.Y))
                 e5.GetComponent<SpriteSheetComponent>().flipY = !e5.GetComponent<SpriteSheetComponent>().flipY;
+
+            if (GetWindow().inputManager.IsMouseButtonPressed(Inputs.MouseButton.LEFT))
+                e6.GetComponent<Physics2DComponent>().SetPosition(GetWindow().inputManager.GetMousePosition());
         }
 
         public override void Load()
--- SharpEngine2/Scene.cs
+++ SharpEngine2/Scene.cs
@@ -1,4 +1,6 @@
 ﻿using System.Collections.Generic;
+using tainicom.Aether.Physics2D.Dynamics;
+using tainicom.Aether.Physics2D.Common;
 
 namespace SE2
 {
@@ -9,14 +11,20 @@ namespace SE2
         internal List<Widgets.Widget> widgets;
         internal int _entityCount;
         internal int _widgetCount;
+        internal World world;
 
-        public Scene()
+        public Scene(int gravity = -200)
         {
             window = null;
             entities = new List<Entities.Entity>();
             widgets = new List<Widgets.Widget>();
             _entityCount = 0;
             _widgetCount = 0;
+
+            world = new World(new Vector2(0, gravity));
+            world.ContactManager.VelocityConstraintsMultithreadThreshold = 256;
+            world.ContactManager.PositionConstraintsMultithreadThreshold = 256;
+            world.ContactManager.CollideMultithreadThreshold = 256;
         }
 
         public virtual List<Entities.Entity> GetEntities() => entities;
@@ -85,6 +93,8 @@ namespace SE2
 
         public virtual void Update(double deltaTime)
         {
+            world.Step((float)deltaTime);
+
             foreach (Entities.Entity e in entities)
                 e.Update(deltaTime);
 
