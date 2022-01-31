69d09364c57ca47829bdc2a4d7c9c87c363444a2
Alexis Huvier <alexis.huvier@alumni.univ-avignon.fr>
2022-01-31 17:00:44 +0100

    Update MyWorld.cs

--- SE2Test/MyWorld.cs
+++ SE2Test/MyWorld.cs
@@ -1,54 +1,49 @@
 ﻿using SE2;
 using SE2.Entities;
 using SE2.Components;
-using SE2.Widgets;
 using SE2.Utils;
-using System.Collections.Generic;
 
 namespace SE2Test
 {
     class MyWorld: Scene
     {
-        readonly Entity e5;
-        readonly Entity e6;
-
         public MyWorld(): base()
         {
-            Entity e2 = new Entity();
-            e2.AddComponent(new TransformComponent(new Vec3(600, 450)));
-            e2.AddComponent(new TileMapComponent("tiled/map.tmx"));
-            AddEntity(e2);
+            Entity e8 = new Entity();
+            e8.AddComponent(new TransformComponent(new Vec3(600, 700), 0));
+            e8.AddComponent(new PolygonComponent(new float[] { 
+                50f, 50f, 0f, 
+                -50f, -50f, 0f, 
+                -50f, 50f, 0f 
+            }, Color.RED));
+            AddEntity(e8);
 
-            e5 = new Entity();
-            e5.AddComponent(new TransformComponent(new Vec3(600, 450), new Vec3(3)));
-            e5.AddComponent(new SpriteSheetComponent("spritesheet", new Vec2(32), new Dictionary<string, List<int>>() { { "idle", new List<int>() { 13, 14, 15, 16, 17, 18, 19, 20 } } }, "idle", 150));
-            e5.AddComponent(new ControlComponent(ControlType.FOURDIRECTION));
-            AddEntity(e5);
+            Entity e9 = new Entity();
+            e9.AddComponent(new TransformComponent(new Vec3(600, 600), 0));
+            e9.AddComponent(new PolygonComponent(new float[] {
+                50f, 50f, 0f,
+                50f, -50f, 0f,
+                -50f, -50f, 0f,
+                -50f, 50f, 0f
+            }, Color.BLUE));
+            AddEntity(e9);
 
-            Entity e7 = new Entity();
-            e7.AddComponent(new TransformComponent(new Vec3(600, 450), 10));
-            e7.AddComponent(new RectComponent(new Vec3(400, 100), Color.BLUE));
-            e7.AddComponent(new PhysicsComponent(new Vec2(400, 100), 1, bodyType: tainicom.Aether.Physics2D.Dynamics.BodyType.Kinematic));
-            AddEntity(e7);
-
-            e6 = new Entity();
-            e6.AddComponent(new TransformComponent(new Vec3(600, 700), 35));
-            e6.AddComponent(new CircleComponent(20f, Color.RED, 20));
-            e6.AddComponent(new PhysicsComponent(20f, density: 10f));
-            AddEntity(e6);
-
-            AddWidget(new Image(new Vec3(100, 100), new Vec3(1), "awesomeface"));
-            AddWidget(new Label(new Vec3(100, 100), new Vec3(1), "Heyo !", "basic"));
+            Entity e10 = new Entity();
+            e10.AddComponent(new TransformComponent(new Vec3(600, 500), 0));
+            e10.AddComponent(new PolygonComponent(new float[] {
+                50f, 50f, 0f,
+                75f, 0f, 0f, 
+                50f, -50f, 0f,
+                -50f, -50f, 0f,
+                -50f, 50f, 0f
+            }, Color.GREEN));
+            AddEntity(e10);
         }
 
         public override void Update(double deltaTime)
         {
             base.Update(deltaTime);
 
-            if (GetWindow().inputManager.IsKeyPressed(Inputs.Key.X))
-                e5.GetComponent<SpriteSheetComponent>().flipX = !e5.GetComponent<SpriteSheetComponent>().flipX;
-            if (GetWindow().inputManager.IsKeyPressed(Inputs.Key.Y))
-                e5.GetComponent<SpriteSheetComponent>().flipY = !e5.GetComponent<SpriteSheetComponent>().flipY;
             if (GetWindow().inputManager.IsKeyPressed(Inputs.Key.M))
             {
                 System.Console.WriteLine("MP3");
@@ -64,16 +59,11 @@ namespace SE2Test
                 System.Console.WriteLine("WAV");
                 GetWindow().soundManager.PlaySound("wav");
             }
-
-            if (GetWindow().inputManager.IsMouseButtonPressed(Inputs.MouseButton.LEFT))
-                e6.GetComponent<PhysicsComponent>().SetPosition(GetWindow().inputManager.GetMousePosition());
         }
 
         public override void Load()
         {
             base.Load();
-
-            GetWindow().GetCamera().follow = e5;
         }
     }
 }
