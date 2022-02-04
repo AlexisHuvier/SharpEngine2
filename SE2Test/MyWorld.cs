812666da48c20d437fad3870de1e6c8036626120
Alexis Huvier <alexis.huvier@alumni.univ-avignon.fr>
2022-02-04 11:45:08 +0100

    Update MyWorld.cs

--- SE2Test/MyWorld.cs
+++ SE2Test/MyWorld.cs
@@ -10,6 +10,9 @@ namespace SE2Test
         {
             AddWidget(new Button(new Vec3(200, 200, 0), new Vec3(200, 50, 1), "HEYO", "basic", null));
             AddWidget(new TexturedButton(new Vec3(200, 400, 0), new Vec3(200, 50, 1), "HEYO", "basic", new string[] { "awesomeface", "container", "container" }, null));
+            AddWidget(new ProgressBar(new Vec3(200, 600, 0), new Vec3(200, 50), Color.AQUA, 75));
+            AddWidget(new TexturedProgressBar(new Vec3(500, 600), new Vec3(200, 50), new string[] { "awesomeface", "container" }));
+            AddWidget(new LineEdit(new Vec3(550, 400), new Vec3(300, 50), "Heyo", "basic"));
         }
 
         public override void Update(double deltaTime)
