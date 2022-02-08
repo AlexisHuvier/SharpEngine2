2905baa54e311b3867ea26cb3e0d41574bc349b5
Alexis Huvier <alexis.huvier@alumni.univ-avignon.fr>
2022-02-09 00:42:46 +0100

    Create Lang.cs

---
+++ SharpEngine2/Utils/Lang.cs
@@ -0,0 +1,43 @@
+﻿using System.Diagnostics;
+using System.IO;
+using System.Collections.Generic;
+using System;
+
+namespace SE2.Utils
+{
+    public class Lang
+    {
+        private Dictionary<string, string> translations;
+        private string _file;
+        public string file
+        {
+            get => _file;
+            set
+            {
+                _file = value;
+                translations.Clear();
+                if (File.Exists(value))
+                {
+                    foreach (string line in File.ReadAllLines(value))
+                    {
+                        if (line.Split(": ").Length == 2)
+                            translations.Add(line.Split(": ")[0], line.Split(": ")[1].Replace("\n", ""));
+                        else
+                            Trace.WriteLine($"[ERROR] Unknown format of Lang : {line} (Use : 'key: value')");
+                    }
+                }
+                else
+                    Trace.WriteLine($"[ERROR] Lang file doesn't exist : {value}");
+            }
+        }
+
+        public Lang(string file)
+        {
+            translations = new Dictionary<string, string>();
+            this.file = file;
+        }
+
+        public string GetTranslation(string key, object[] objects) => translations.ContainsKey(key) ? String.Format(translations[key], objects) : $"Unknown translation : {key}";
+        public string GetTranslation(string key) => translations.ContainsKey(key) ? translations[key] : $"Unknown translation : {key}";
+    }
+}
