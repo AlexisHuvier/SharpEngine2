80228b442ddf5ee74efb65d6a1d8f1feb443cc12
Alexis Huvier <alexis.huvier@alumni.univ-avignon.fr>
2022-02-16 17:16:44 +0100

    Create YamlSave.cs

---
+++ SharpEngine2/Utils/YamlSave.cs
@@ -0,0 +1,12 @@
+﻿using System.IO;
+using YamlDotNet.Serialization;
+using YamlDotNet.Serialization.NamingConventions;
+
+namespace SE2.Utils
+{
+    class YamlSave
+    {
+        public static void Save<T>(T obj, string file) => File.WriteAllText(file, new SerializerBuilder().WithNamingConvention(CamelCaseNamingConvention.Instance).Build().Serialize(obj));
+        public static T Read<T>(string file) => new DeserializerBuilder().WithNamingConvention(CamelCaseNamingConvention.Instance) .Build().Deserialize<T>(File.ReadAllText(file));
+    }
+}
